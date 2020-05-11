54354856b8e5369920f32663b30c603e6f8d8c8d
jojiang <jojiang@qq.com>
2020-05-11 09:49:20 +0800

    first commit

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/consumer")
+public class OrderController {
+    private static final String PAYMENT_URL = "http://localhost:8001/payment";
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/get/"+id, CommonResult.class);
+    }
+}
